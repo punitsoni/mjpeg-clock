8cdd2f7666af20a18b3728c8ff614544bf6ff34f
Punit Soni <punitxsmart@gmail.com>
2018-11-27 14:15:44 -0800

    simple clock animation with opencv and qt

--- copper/copper_main.cc
+++ copper/copper_main.cc
@@ -1,7 +1,11 @@
+#include <QtCore/QTimer>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
 #include <iostream>
+#include "absl/strings/str_format.h"
+#include "mainwindow.h"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/opencv.hpp"
 
@@ -10,43 +14,36 @@ using std::endl;
 
 using namespace cv;
 
-void hello_opencv() {
-  namedWindow("Output", 1);
-  Mat output = Mat::zeros(480, 640, CV_8UC3);
-  // write text on the matrix:
-  putText(output, "Hello World :)", Point(15, 70), FONT_HERSHEY_PLAIN, 3,
-          Scalar(0, 255, 0), 4);
-  // display the image:
-  imshow("Output", output);
-  // wait for the user to press any key:
-  waitKey(0);
+int hello_opencv_qt(int argc, char **argv) {
+  QApplication app(argc, argv);
+
+  MainWindow win;
+
+  // QWidget window;
+  // window.resize(640, 480);
+  // // Disable resizing.
+  // window.setFixedSize(window.size());
+  // window.setWindowTitle(QApplication::translate("toplevel", "Copper"));
+
+  // QLabel label(&window);
+  // label.setAlignment(Qt::AlignCenter);
+
+  // // opencv image
+  // Mat mat = Mat::zeros(480, 640, CV_8UC3);
+
+  // generate_frame(&mat, std::chrono::milliseconds(1234));
+
+  // label.setPixmap(QPixmap::fromImage(
+  //     QImage(mat.data, mat.cols, mat.rows, mat.step,
+  //     QImage::Format_RGB888)));
+
+  // QTimer timer(nullptr);
+
+  // window.show();
+  return app.exec();
 }
 
 int main(int argc, char **argv) {
   cout << "Copper." << endl;
-  QApplication app(argc, argv);
-  QWidget window;
-  window.resize(640, 480);
-  // Disable resizing.
-  window.setFixedSize(window.size());
-  window.setWindowTitle(QApplication::translate("toplevel", "Copper"));
-
-  QLabel label(&window);
-  // label.setFrameStyle(QFrame::Panel | QFrame::Sunken);
-  label.setAlignment(Qt::AlignCenter);
-
-  // label.setPixmap(QPixmap::fromImage(img).scaled(
-  //     label.size(), Qt::KeepAspectRatio, Qt::FastTransformation));
-
-  // opencv image
-  Mat mat = Mat::zeros(480, 640, CV_8UC3);
-  // write text on the matrix:
-  putText(mat, "Hello World :)", Point(15, 70), FONT_HERSHEY_PLAIN, 3,
-          Scalar(0, 255, 0), 4);
-
-  label.setPixmap(QPixmap::fromImage(
-      QImage(mat.data, mat.cols, mat.rows, mat.step, QImage::Format_RGB888)));
-
-  window.show();
-  return app.exec();
+  return hello_opencv_qt(argc, argv);
 }
